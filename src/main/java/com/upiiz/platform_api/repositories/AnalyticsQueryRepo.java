3884a12726fc77174c503dcfc97cc59409d311fa
AdminMel <74563324+AdminMel@users.noreply.github.com>
2026-04-30 17:34:47 -0600

    si no jala bye 2

--- src/main/java/com/upiiz/platform_api/repositories/AnalyticsQueryRepo.java
+++ src/main/java/com/upiiz/platform_api/repositories/AnalyticsQueryRepo.java
@@ -12,15 +12,15 @@ public interface AnalyticsQueryRepo extends JpaRepository<TeacherEvaluation, UUI
 
     @Query(value = """
         SELECT
-            category_id,
-            category_name,
-            category_code,
-            subarea_id,
-            subarea_name,
-            total_events,
-            weighted_score,
-            unique_users,
-            last_event_at
+            category_id AS "categoryId",
+            category_code AS "categoryCode",
+            category_name AS "categoryName",
+            subarea_id AS "subareaId",
+            subarea_name AS "subareaName",
+            total_events::bigint AS "totalEvents",
+            weighted_score::bigint AS "weightedScore",
+            unique_users::bigint AS "uniqueUsers",
+            last_event_at AS "lastEventAt"
         FROM vw_admin_topic_interest
         ORDER BY weighted_score DESC, total_events DESC
         """, nativeQuery = true)
@@ -28,15 +28,15 @@ public interface AnalyticsQueryRepo extends JpaRepository<TeacherEvaluation, UUI
 
     @Query(value = """
         SELECT
-            category_id,
-            category_name,
-            category_code,
-            subarea_id,
-            subarea_name,
-            total_reports,
-            avg_difficulty,
-            affected_students,
-            last_report_at
+            category_id AS "categoryId",
+            category_code AS "categoryCode",
+            category_name AS "categoryName",
+            subarea_id AS "subareaId",
+            subarea_name AS "subareaName",
+            total_reports::bigint AS "totalReports",
+            avg_difficulty::numeric AS "avgDifficulty",
+            affected_students::bigint AS "affectedStudents",
+            last_report_at AS "lastReportAt"
         FROM vw_admin_topic_difficulty
         ORDER BY avg_difficulty DESC, total_reports DESC
         """, nativeQuery = true)
@@ -44,21 +44,21 @@ public interface AnalyticsQueryRepo extends JpaRepository<TeacherEvaluation, UUI
 
     @Query(value = """
         SELECT
-            teacher_id,
-            full_name,
-            email_inst,
-            total_evaluations,
-            avg_clarity,
-            avg_knowledge,
-            avg_support,
-            avg_punctuality,
-            avg_global_score,
-            total_forum_posts,
-            total_forum_threads,
-            total_appointments_created,
-            completed_appointments,
-            total_video_meetings,
-            ended_video_meetings
+            teacher_id AS "teacherId",
+            full_name AS "fullName",
+            email_inst AS "emailInst",
+            total_evaluations::bigint AS "totalEvaluations",
+            avg_clarity::numeric AS "avgClarity",
+            avg_knowledge::numeric AS "avgKnowledge",
+            avg_support::numeric AS "avgSupport",
+            avg_punctuality::numeric AS "avgPunctuality",
+            avg_global_score::numeric AS "avgGlobalScore",
+            total_forum_posts::bigint AS "totalForumPosts",
+            total_forum_threads::bigint AS "totalForumThreads",
+            total_appointments_created::bigint AS "totalAppointmentsCreated",
+            completed_appointments::bigint AS "completedAppointments",
+            total_video_meetings::bigint AS "totalVideoMeetings",
+            ended_video_meetings::bigint AS "endedVideoMeetings"
         FROM vw_teacher_performance
         ORDER BY avg_global_score DESC, total_evaluations DESC
         """, nativeQuery = true)
@@ -66,21 +66,21 @@ public interface AnalyticsQueryRepo extends JpaRepository<TeacherEvaluation, UUI
 
     @Query(value = """
         SELECT
-            teacher_id,
-            full_name,
-            email_inst,
-            total_evaluations,
-            avg_clarity,
-            avg_knowledge,
-            avg_support,
-            avg_punctuality,
-            avg_global_score,
-            total_forum_posts,
-            total_forum_threads,
-            total_appointments_created,
-            completed_appointments,
-            total_video_meetings,
-            ended_video_meetings
+            teacher_id AS "teacherId",
+            full_name AS "fullName",
+            email_inst AS "emailInst",
+            total_evaluations::bigint AS "totalEvaluations",
+            avg_clarity::numeric AS "avgClarity",
+            avg_knowledge::numeric AS "avgKnowledge",
+            avg_support::numeric AS "avgSupport",
+            avg_punctuality::numeric AS "avgPunctuality",
+            avg_global_score::numeric AS "avgGlobalScore",
+            total_forum_posts::bigint AS "totalForumPosts",
+            total_forum_threads::bigint AS "totalForumThreads",
+            total_appointments_created::bigint AS "totalAppointmentsCreated",
+            completed_appointments::bigint AS "completedAppointments",
+            total_video_meetings::bigint AS "totalVideoMeetings",
+            ended_video_meetings::bigint AS "endedVideoMeetings"
         FROM vw_teacher_performance
         WHERE teacher_id = :teacherId
         """, nativeQuery = true)
@@ -88,16 +88,16 @@ public interface AnalyticsQueryRepo extends JpaRepository<TeacherEvaluation, UUI
 
     @Query(value = """
         SELECT
-            teacher_id,
-            teacher_name,
-            category_id,
-            category_name,
-            category_code,
-            subarea_id,
-            subarea_name,
-            total_difficulty_events,
-            avg_difficulty,
-            last_event_at
+            teacher_id AS "teacherId",
+            teacher_name AS "teacherName",
+            category_id AS "categoryId",
+            category_code AS "categoryCode",
+            category_name AS "categoryName",
+            subarea_id AS "subareaId",
+            subarea_name AS "subareaName",
+            total_difficulty_events::bigint AS "totalDifficultyEvents",
+            avg_difficulty::numeric AS "avgDifficulty",
+            last_event_at AS "lastEventAt"
         FROM vw_teacher_improvement_areas
         ORDER BY avg_difficulty DESC, total_difficulty_events DESC
         """, nativeQuery = true)
@@ -105,16 +105,16 @@ public interface AnalyticsQueryRepo extends JpaRepository<TeacherEvaluation, UUI
 
     @Query(value = """
         SELECT
-            teacher_id,
-            teacher_name,
-            category_id,
-            category_name,
-            category_code,
-            subarea_id,
-            subarea_name,
-            total_difficulty_events,
-            avg_difficulty,
-            last_event_at
+            teacher_id AS "teacherId",
+            teacher_name AS "teacherName",
+            category_id AS "categoryId",
+            category_code AS "categoryCode",
+            category_name AS "categoryName",
+            subarea_id AS "subareaId",
+            subarea_name AS "subareaName",
+            total_difficulty_events::bigint AS "totalDifficultyEvents",
+            avg_difficulty::numeric AS "avgDifficulty",
+            last_event_at AS "lastEventAt"
         FROM vw_teacher_improvement_areas
         WHERE teacher_id = :teacherId
         ORDER BY avg_difficulty DESC, total_difficulty_events DESC
